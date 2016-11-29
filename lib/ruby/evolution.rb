c296a80ab96b69eff129f93a551c884b8d194818
Alex Manelis <amanelis@users.noreply.github.com>
2016-11-29 14:41:36 -0800

    Update evolution.rb

--- lib/ruby/evolution.rb
+++ lib/ruby/evolution.rb
@@ -3,12 +3,4 @@ require './evolution/fitness_calc'
 require './evolution/individual'
 require './evolution/population'
 
-module Evolution
-  class Client
-    def initialize
-
-    end
-  end
-end
-
 Evolution::Population.new 50, true
